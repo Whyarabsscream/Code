bdabf65ab2fa2a4bef3578d2ac953d7e5888fe52
Whyarabsscream <23ajuckiewicz.dbc@lmetb.ie>
2024-04-10 10:02:59 +0100

    Code

---
+++ index.js
@@ -0,0 +1,87 @@
+var express = require("express");
+var app = express();
+var mysql = require('mysql');
+app.set('view engine', 'ejs'); // Set the template engine 
+
+var bodyParser = require("body-parser") // call body parser module and make use of it
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+
+// ******************************** Start of SQL **************************************** //
+// First we need to tell the application where to find the database
+const db = mysql.createConnection({
+host: '127.0.0.1',
+    user: 'root',
+    port: '3306',
+    password: 'root',
+    database: 'aleksy'
+ });
+
+// Next we need to create a connection to the database
+
+db.connect((err) =>{
+     if(err){
+        console.log("Go back and check the connection details. Something is wrong.")
+    } 
+     else{
+        console.log('Looking good the database connected')
+    }
+})
+
+
+// **********************************  Code from here **************************
+app.get('/', function(req,res){
+    let sql = 'SELECT * FROM Phones';
+    let query = db.query(sql, (err,result) => {
+        if(err) throw err;
+        console.log(result);
+        res.render('home', {result})   
+    });
+    
+})
+
+
+app.get('/add', function(req,res){
+
+    res.render('add')
+
+
+})
+
+
+app.post('/add', function(req,res){
+    let sql = 'insert into Phones (make, model, price, image) values (?, ?, ?, ?)';
+    let query = db.query(sql,[req.body.make, req.body.model, req.body.price, req.body.image], (err,result) => {
+        if(err) throw err;
+        console.log(result);
+        res.redirect( '/')   
+    });
+      
+    
+})
+app.get('/individual/:id', function(req,res){
+    let sql = 'SELECT * FROM Phones WHERE id = ?';
+    let query = db.query(sql,[req.params.id], (err,result) => {
+        if(err) throw err;
+        console.log(result);
+        res.render('individual', {result})   
+    });
+    
+})
+//Calls All folders
+app.use(express.static("views")); 
+app.use(express.static("images")); 
+app.use(express.static("style")); 
+
+
+
+
+
+
+// **********************************  Code to here **************************
+
+app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0" , function(){
+  console.log("New Full Demo is Live")
+})
+
